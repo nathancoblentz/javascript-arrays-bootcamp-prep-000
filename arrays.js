42619177421599c2952ead1a4d015bd5ed4839a7
nathancoblentz <nathancoblentz@gmail.com>
2017-10-10 11:12:40 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, item) {
+  const array = chocolateBars
+  var item = "Milky Way"
+  const moreChocolateBars = [item, ...array]
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, item) {
+  var array = chocolateBars
+  var item = "Milky Way"
+  chocolateBars.unshift(item)
+
+}
+
+
+function destructivelyAddElementToEndOfArray(array, item) {
+  var array = chocolateBars
+  var item = "Milky Way"
+
+}
+
+  function destructivelyAddElementToEndOfArray(array, item) {
+    var array = chocolateBars
+    var item = "Milky Way"
+
+}
